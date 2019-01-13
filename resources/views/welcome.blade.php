1d896fd008194b1546b0768a488812e380bab534
hamza-kay <hamzahk1991@gmail.com>
2019-01-13 22:26:42 +0000

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CSS for Backend Devs</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+    <link rel="stylesheet" href="/css/app.css">
+
+    <link rel="stylesheet" href="/css/noramlise.css">
+
+
+</head>
+
+<body>
+
+<header>
+
+    <div class="container">
+    <div>
+
+        <h1>XCasts</h1>
+
+        <a href="#">Sign In</a>
+
+    </div>
+
+    <nav>
+
+        <a href="#">Catalog</a>
+
+        <a href="#">Series</a>
+
+        <a href="#">Podcast</a>
+        r
+        <a href="#">Discussions</a>
+
+
+    </nav>
+
+        </div>
+</header>
+
+
+
+</body>
+
+
+</html>
